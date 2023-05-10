b149254c6a1e7342ac6464c2c2f0a7ae5f6019c4
Himashana <himashanasuraweera5@gmail.com>
2023-05-10 22:22:21 +0530

    Backup : Finalized version 1

--- HealthUser.php
+++ HealthUser.php
@@ -45,6 +45,29 @@
             return $UserDetails;
         }
 
+        function GetHealthUserById($id){
+            $dbconnection = new DBconnect();
+            $dbconnection->MakeConn();
+            
+            $query = 'SELECT userId, fullName, userPrivileges, role, MOHOffice, phoneNo, NICNO, address FROM HealthUser
+                        WHERE userId=' . $id;
+
+            $results = $dbconnection->ExecuteQuery($query);
+            $UserDetails = array();
+
+            if($results){
+                                    
+                while($r = mysqli_fetch_assoc($results)){
+                    array_push($UserDetails, $r['userId'], $r['fullName'], $r['userPrivileges'], $r['role'], $r['MOHOffice'], $r['phoneNo'], $r['NICNO'], $r['address']);
+                }
+                
+            }
+
+            $dbconnection->CloseConn();
+
+            return $UserDetails;
+        }
+
         function GetHealthUsers(){
             $dbconnection = new DBconnect();
             $dbconnection->MakeConn();
